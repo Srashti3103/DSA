cf4686657a8f0f5efd6250d02a987cfb344b2a85
Srashti3103 <srashtipatel31@gmail.com>
2025-12-18 00:25:44 +0530

    Delete_last

---
+++ LINKED_LIST/Delete_last.cpp
@@ -0,0 +1,67 @@
+//Delete last node
+#include<bits/stdc++.h>
+using namespace std;
+struct Node 
+{
+	int data;
+	Node *next ;
+};
+Node *first , *temp ,*ttemp , *p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void createfirst()
+{
+	first = new Node;
+	first -> next = NULL;
+	cout<< "Enter data for the first node :";
+	cin>>first->data;
+}
+void AddNode()
+{ 
+	temp = first;
+	while(temp->next!=NULL)
+	{
+		temp = temp -> next ;
+	}
+		ttemp = new Node;
+		cout<< "Enter the data for new node :";
+		cin>>ttemp -> data ;
+		ttemp -> next = NULL;
+		temp ->next = ttemp;
+}
+void Del_last()
+{
+	temp = first ;
+	while(temp -> next !=NULL)
+	{
+		ttemp = temp;
+		temp = temp -> next;
+	}
+	ttemp->next=NULL;
+	delete temp;
+}
+void disp()
+{
+	temp = first;
+	while(temp!= NULL)
+	{
+		cout<<temp->data<<endl;
+		temp = temp -> next;
+	}
+}
+int main()
+{
+	init();
+	createfirst();
+	AddNode();
+	AddNode();
+	AddNode();
+	AddNode();
+	cout<< "Linked list :"<<endl;
+	disp();
+	cout<< "after delete last node "<<endl;
+	Del_last();
+	disp();
+}
