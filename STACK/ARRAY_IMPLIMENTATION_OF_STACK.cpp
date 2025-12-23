150520c3e94381510aa3087bab31674fdcd21d92
Srashti3103 <srashtipatel31@gmail.com>
2025-12-23 21:31:26 +0530

    ARRAY_IMPLIMENTATION_OF_STACK

---
+++ STACK/ARRAY_IMPLIMENTATION_OF_STACK.cpp
@@ -0,0 +1,59 @@
+//ARRAY IMPLIMENTATION OF STACK 
+#include<iostream>
+#define max 5
+using namespace std;
+struct stack{
+	int data[max];
+	int top;
+};
+stack *p,s1;
+void init(){
+	p=&s1;
+	p->top=-1;
+}
+int Empty(){
+	if(p->top==-1){
+		cout<<"Stack is empty"<<endl;
+		return 1;
+	}else{
+		return 0;
+	}
+}
+int Full(){
+	if(p->top==max-1){
+		cout<<"Stack is full"<<endl;
+		return 1;
+	}else{
+		return 0;
+	}
+}
+void push(){
+	if(Full()){
+		return;
+	}
+	p->top++;
+	cout<<"Enter data";
+	cin>>p->data[p->top];
+}
+int pop(){
+	if(Empty()){
+		return 1;
+	}
+	cout<<p->data[p->top]<<endl;
+	p->top--;
+	return 0;
+}
+int main(){
+	init();
+	push();
+	push();
+	push();
+	push();
+	push();
+	push();
+	pop();
+	pop();
+	pop();
+	pop();
+	pop();
+}
