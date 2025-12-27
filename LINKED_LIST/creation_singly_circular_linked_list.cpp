b1b4960e5ddfb8207935a806ec57c0885a9a9b00
Srashti3103 <srashtipatel31@gmail.com>
2025-12-27 21:32:34 +0530

    creation_singly_circular_linked_list

---
+++ LINKED_LIST/creation_singly_circular_linked_list.cpp
@@ -0,0 +1,52 @@
+//Creation of a Singly Circular Linked List
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;	
+};
+Node *first, *temp ,*ttemp;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void create_first()
+{
+	first = new Node;
+	first -> next = first;
+	cout<<"Enter the data in first node :"<<endl;
+	cin>>first->data;
+}
+void Add_new_Node()
+{
+	temp = first;
+	while(temp->next!=first)
+	{
+		temp = temp -> next;
+	}
+	ttemp= new Node;
+	cout<<"Entet the data in new Node :"<<endl;
+	cin>>ttemp ->data;
+	ttemp ->next = first;
+	temp ->next = ttemp;
+}
+void disp()
+{
+	temp =first;
+	do{
+		cout<<temp->data<<endl;
+		temp = temp->next;
+	}while(temp!=first);
+}
+int main()
+{
+	init();
+    create_first();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	cout<<"Linked List :"<<endl;
+	disp();
+}
