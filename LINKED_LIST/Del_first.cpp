fe0516e43fb3a89e9683d24545042929e94582cf
Srashti3103 <srashtipatel31@gmail.com>
2025-12-28 23:14:50 +0530

    Del_first

---
+++ LINKED_LIST/Del_first.cpp
@@ -0,0 +1,64 @@
+//Delete first node
+#include<bits/stdc++.h>
+using namespace std;
+struct Node 
+{
+	int data;
+	Node *next ;
+};
+Node *first , *temp ,*ttemp , *p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void createfirst()
+{
+	first = new Node;
+	first -> next = NULL;
+	cout<< "Enter data for the first node :";
+	cin>>first->data;
+}
+void AddNode()
+{ 
+	temp = first;
+	while(temp->next!=NULL)
+	{
+		temp = temp -> next ;
+	}
+		ttemp = new Node;
+		cout<< "Enter the data for new node :";
+		cin>>ttemp -> data ;
+		ttemp -> next = NULL;
+		temp ->next = ttemp;
+}
+void Del_first()
+{
+	temp = first ;
+	ttemp = temp -> next;
+	temp -> next = NULL;
+	delete temp ;
+	first = ttemp ;	
+}
+void disp()
+{
+	temp = first;
+	while(temp!= NULL)
+	{
+		cout<<temp->data<<endl;
+		temp = temp -> next;
+	}
+}
+int main()
+{
+	init();
+	createfirst();
+	AddNode();
+	AddNode();
+	AddNode();
+	AddNode();
+	cout<< "Linked list :"<<endl;
+	disp();
+	cout<< "after delete first node "<<endl;
+	Del_first();
+	disp();
+}
