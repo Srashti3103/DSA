776514d677388535a0aaba4650d63d6ec752bf0a
Srashti3103 <srashtipatel31@gmail.com>
2025-12-31 18:52:54 +0530

    Delete_node_before_in_singly_linked_list

---
+++ LINKED_LIST/Delete_node_before_in_singly_linked_list.cpp
@@ -0,0 +1,68 @@
+//Delete a node before a given data 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;	
+};
+Node *first, *temp ,*ttemp,*p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void create_first()
+{
+	first = new Node;
+	first -> next = first;
+	cout<<"Enter the data in first node :"<<endl;
+	cin>>first->data;
+}
+void Add_new_Node()
+{
+	temp = first;
+	while(temp->next!=first)
+	{
+		temp = temp -> next;
+	}
+	ttemp= new Node;
+	cout<<"Entet the data in new Node :"<<endl;
+	cin>>ttemp ->data;
+	ttemp ->next = first;
+	temp ->next = ttemp;
+}
+void Del_Node_Before(int x)
+{
+	temp = first;
+	while(temp->next->data!=x)
+	{
+		ttemp=temp;
+		temp=temp->next;
+	}
+	p=temp->next;
+	ttemp->next=p;
+	temp->next=NULL;
+	delete temp;
+}
+void disp()
+{
+	temp =first;
+	do{
+		cout<<temp->data<<endl;
+		temp = temp->next;
+	}while(temp!=first);
+}
+int main()
+{
+	init();
+    create_first();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	cout<<"Linked List :"<<endl;
+	disp();
+	Del_Node_Before(30);
+	cout<<"Linked List after delete a node before a given data :"<<endl;
+	disp();
+}
