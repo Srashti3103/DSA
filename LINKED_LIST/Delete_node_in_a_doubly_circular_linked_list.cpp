753d81e3f1a0ae09e595f40e4d30250ea354b08b
Srashti3103 <srashtipatel31@gmail.com>
2026-01-05 19:57:17 +0530

    Delete_node_in_a_doubly_circular_linked_list.cpp

---
+++ LINKED_LIST/Delete_node_in_a_doubly_circular_linked_list.cpp
@@ -0,0 +1,68 @@
+//Delete a given node in a singly circular linked list 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;	
+};
+Node *first, *temp ,*ttemp,*p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void create_first()
+{
+	first = new Node;
+	first -> next = first;
+	cout<<"Enter the data in first node :"<<endl;
+	cin>>first->data;
+}
+void Add_new_Node()
+{
+	temp = first;
+	while(temp->next!=first)
+	{
+		temp = temp -> next;
+	}
+	ttemp= new Node;
+	cout<<"Entet the data in new Node :"<<endl;
+	cin>>ttemp ->data;
+	ttemp ->next = first;
+	temp ->next = ttemp;
+}
+void Del_Node(int x)
+{
+	temp = first;
+	while(temp -> data!=x)
+	{
+		ttemp= temp;
+		temp=temp->next;
+	}
+	p=temp->next;
+	ttemp->next=p;
+	temp->next=NULL;
+	delete temp;
+}
+void disp()
+{
+	temp =first;
+	do{
+		cout<<temp->data<<endl;
+		temp = temp->next;
+	}while(temp!=first);
+}
+int main()
+{
+	init();
+    create_first();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	cout<<"Linked List :"<<endl;
+	disp();
+	Del_Node(30);
+	cout<<"Linked List after delete a given node :"<<endl;
+	disp();
+}
