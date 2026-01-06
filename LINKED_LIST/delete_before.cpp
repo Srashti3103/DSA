c58b9797fce54bd5ca0adf7eaadd276c48ab0f8b
Srashti3103 <srashtipatel31@gmail.com>
2026-01-06 19:33:44 +0530

    delete_before.cpp

---
+++ LINKED_LIST/delete_before.cpp
@@ -0,0 +1,69 @@
+//Delete a node before a given data
+#include<bits/stdc++.h>
+using namespace std;
+struct Node 
+{
+	int data;
+	Node *next ;
+};
+Node *first , *temp ,*ttemp , *p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void createfirst()
+{
+	first = new Node;
+	first -> next = NULL;
+	cout<< "Enter data for the first node :";
+	cin>>first->data;
+}
+void AddNode()
+{ 
+	temp = first;
+	while(temp->next!=NULL)
+	{
+		temp = temp -> next ;
+	}
+		ttemp = new Node;
+		cout<< "Enter the data for new node :";
+		cin>>ttemp -> data ;
+		ttemp -> next = NULL;
+		temp ->next = ttemp;
+}
+void Del_Before(int x)
+{
+	temp = first;
+	while(temp->next->data!=x)
+	{
+		ttemp = temp;
+		temp = temp->next;
+	}
+	p = temp ->next;
+    temp ->next = NULL;
+    ttemp->next = p;
+	delete temp;
+}
+void disp()
+{
+	temp = first;
+	while(temp!= NULL)
+	{
+		cout<<temp->data<<endl;
+		temp = temp -> next;
+	}
+}
+int main()
+{
+	init();
+	createfirst();
+	AddNode();
+	AddNode();
+	AddNode();
+	AddNode();
+	cout<< "Linked list :"<<endl;
+	disp();
+	cout<< "delete node before given data "<<endl;
+	Del_Before(30);
+	disp();
+}
