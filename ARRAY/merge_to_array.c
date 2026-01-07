f4a0ef296b30500e750c3ec6da3c48564a56424c
Srashti3103 <srashtipatel31@gmail.com>
2026-01-07 17:40:50 +0530

    To merge two arrays into one

---
+++ ARRAY/merge_to_array.c
@@ -0,0 +1,12 @@
+//merging two array in a single array 
+#include<stdio.h>
+int main()
+{
+    int a1[5]={2,4,6,8,10};
+    int a2[5]={1,3,4,5,6};
+    int c[10];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0; j<10;j++)
+    }
+}
