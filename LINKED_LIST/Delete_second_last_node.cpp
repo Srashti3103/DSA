2ae63dad1a20106b2e5b9ba55b06193dcd4b001d
Srashti3103 <srashtipatel31@gmail.com>
2026-01-08 22:08:39 +0530

    Delete_second_last_node.cpp

---
+++ LINKED_LIST/Delete_second_last_node.cpp
@@ -0,0 +1,71 @@
+//Delete second last node in a doubly linked list 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next,*prev;
+};
+Node *first,*temp,*ttemp,*p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void create_first()
+{
+	cout<<"Enter the data for first node "<<endl;
+	first = new Node;
+	cin>>first->data;
+	first->next=first->prev=NULL;
+}
+void Add_Node()
+{
+	temp = first;
+	while(temp->next!=NULL)
+	{
+		temp=temp->next;
+	}
+	ttemp = new Node;
+	cout<<"Enter the data for new node "<<endl;
+	cin>>ttemp->data;
+	ttemp->next = NULL;
+	ttemp->prev = temp;
+	temp->next = ttemp;
+}
+void Del_SecondLast()
+{
+	temp = first;
+	while(temp->next->next!=NULL)
+	{
+		ttemp = temp;
+		temp=temp->next;
+	}
+	p=temp->next;
+	ttemp->next=p;
+	p->prev=ttemp;
+	temp->next=temp->prev=NULL;
+	delete temp;
+}
+void disp()
+{
+	temp = first;
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<endl;
+		temp=temp->next;
+	}
+}
+int main()
+{
+	init();
+	create_first();
+	Add_Node();
+	Add_Node();
+	Add_Node();
+	Add_Node();
+	cout<<"Doubly Linked List :"<<endl;
+	disp();
+	Del_SecondLast();
+	cout<<"Linked list after deleting second last node:"<<endl;
+	disp();
+}
