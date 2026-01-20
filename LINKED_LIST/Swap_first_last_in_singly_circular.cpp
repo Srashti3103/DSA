9716faaf59f3ea348dd479cd2933d4dd3fc17ffc
Srashti3103 <srashtipatel31@gmail.com>
2026-01-20 23:22:20 +0530

    Swap_first_last_in_singly_circular.cpp

---
+++ LINKED_LIST/Swap_first_last_in_singly_circular.cpp
@@ -0,0 +1,69 @@
+//Swap First and last node in a singly circular linked list
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;	
+};
+Node *first, *temp ,*ttemp,*p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void create_first()
+{
+	first = new Node;
+	first -> next = first;
+	cout<<"Enter the data in first node :"<<endl;
+	cin>>first->data;
+}
+void Add_new_Node()
+{
+	temp = first;
+	while(temp->next!=first)
+	{
+		temp = temp -> next;
+	}
+	ttemp= new Node;
+	cout<<"Entet the data in new Node :"<<endl;
+	cin>>ttemp ->data;
+	ttemp ->next = first;
+	temp ->next = ttemp;
+}
+void Swap_First_Last_Node()
+{
+	temp=first;
+	while(temp->next!=first)
+	{
+		ttemp=temp;
+		temp=temp->next;
+	}
+	p=first->next;
+	first->next=temp;
+	ttemp->next=first;
+	temp->next=p;
+	first=temp;
+}
+void disp()
+{
+	temp =first;
+	do{
+		cout<<temp->data<<endl;
+		temp = temp->next;
+	}while(temp!=first);
+}
+int main()
+{
+	init();
+    create_first();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	Add_new_Node();
+	cout<<"Linked List :"<<endl;
+	disp();
+	Swap_First_Last_Node();
+	cout<<"Linked List after Swap First and Last node :"<<endl;
+	disp();
+}
