19c3a845101acfbabeb73986087f6c8a3699b4db
Srashti3103 <srashtipatel31@gmail.com>
2026-01-21 12:57:21 +0530

    SWAP_LAST_AND_SECOND_LAST.cpp

---
+++ LINKED_LIST/SWAP_LAST_AND_SECOND_LAST.cpp
@@ -0,0 +1,69 @@
+//Swap last and second last
+#include<bits/stdc++.h>
+using namespace std;
+struct Node 
+{
+	int data;
+	Node *next ;
+};
+Node *first , *temp ,*ttemp , *p;
+void init()
+{
+	first = temp = ttemp = NULL;
+}
+void createfirst()
+{
+	first = new Node;
+	first -> next = NULL;
+	cout<< "Enter data for the first node :";
+	cin>>first->data;
+}
+void AddNode()
+{ 
+	temp = first;
+	while(temp->next!=NULL)
+	{
+		temp = temp -> next ;
+	}
+		ttemp = new Node;
+		cout<< "Enter the data for new node :";
+		cin>>ttemp -> data ;
+		ttemp -> next = NULL;
+		temp ->next = ttemp;
+}
+void Swap_LSL()
+{
+	temp = first ;
+	while(temp->next->next!= NULL)
+	{
+	ttemp = temp;
+	temp = temp -> next;
+	p = temp -> next ;	
+	}
+	p-> next = temp;
+	ttemp -> next = p;
+	temp -> next = NULL;	
+}
+void disp()
+{
+	temp = first;
+	while(temp!= NULL)
+	{
+		cout<<temp->data<<endl;
+		temp = temp -> next;
+	}
+}
+int main()
+{
+	init();
+	createfirst();
+	AddNode();
+	AddNode();
+	AddNode();
+	AddNode();
+	cout<< "Linked list :"<<endl;
+	disp();
+	cout<< "Swap last and second last "<<endl;
+	Swap_LSL();
+	disp();
+}
