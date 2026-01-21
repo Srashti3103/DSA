e47451ebb2eff66cb56b7a040e1341a307294c3a
Srashti3103 <srashtipatel31@gmail.com>
2026-01-21 12:57:06 +0530

    Swap_last_and_second_last_in_doubly_circular_inkied_list.cpp

---
+++ LINKED_LIST/Swap_last_and_second_last_in_doubly_circular_inkied_list.cpp
@@ -0,0 +1,62 @@
+//Swap last and Second last in a doubly circular linked list
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int data;
+	Node *prev,*next;
+};
+Node *first,*temp,*ttemp,*p;
+void init(){
+	first=temp=ttemp=NULL;
+}
+void Create_first(){
+	cout<<"Enter the data for first node:"<<endl;
+	first=new Node;
+	cin>>first->data;
+	first->prev=first->next=first;
+}
+void Add_Node(){
+	temp=first->prev;
+	ttemp=new Node;
+	cout<<"Enter the data for the new Node:"<<endl;
+	cin>>ttemp->data;
+	first->prev=ttemp;
+	ttemp->prev=temp;
+	ttemp->next=first;
+	temp->next=ttemp;
+}
+void Swap_Last_Second_last(){
+	temp=first;
+	while(temp->next->next!=first){
+		ttemp=temp;
+		temp=temp->next;
+	}
+	p=temp->next;
+	first->prev=temp;
+	ttemp->next=p;
+	temp->prev=p;
+	temp->next=first;
+	p->prev=ttemp;
+	p->next=temp;
+}
+void disp(){
+	temp=first;
+	do{
+		cout<<temp->data<<endl;
+		temp=temp->next;
+	}
+	while(temp!=first);
+}
+int main(){
+	init();
+	Create_first();
+	Add_Node();
+	Add_Node();
+	Add_Node();
+	Add_Node();
+	cout<<"Linked List:"<<endl;
+	disp();
+	Swap_Last_Second_last();
+	cout<<"Linked List after Swap last and second last node "<<endl;
+	disp();
+}
